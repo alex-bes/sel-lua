5ecf85b1b5dd3f9ada3c52458897a50c0d21bffe
Alexander Beskrovny <alexander.beskrovny@logicify.com>
2015-09-18 22:13:16 +0200

    default handlers library: allOf and anyOf handlers

--- sel/handlersLibrary.lua
+++ sel/handlersLibrary.lua
@@ -4,15 +4,36 @@ return {
     populateContextByDefaultHandlers = function(context)
 
         context:registerHandler('not', function(arg)
-            print("arg:", arg)
             arg = tostring(arg)
             arg = arg:lower();
-            print("arg:", arg)
             local args = stringSplit(arg, ",");
---            assert(table.getn(args) == 1, "SEL: not handler accepts only one argument")
+            --            assert(table.getn(args) == 1, "SEL: not handler accepts only one argument")
             arg = args[1];
             --TODO: trim arg
             return not (arg == "true")
+        end)
+
+        context:registerHandler('allOf', function(arg)
+            arg = tostring(arg)
+            local args = stringSplit(arg, ",");
+            for k, v in pairs(args) do
+                v = v --TODO: trim it here
+                if v ~= "true" then
+                    return false;
+                end
+            end
+            return true;
+        end)
+        context:registerHandler('anyOf', function(arg)
+            arg = tostring(arg)
+            local args = stringSplit(arg, ",");
+            for k, v in pairs(args) do
+                v = v --TODO: trim it here
+                if v == "true" then
+                    return true;
+                end
+            end
+            return false;
         end)
 
         return context
